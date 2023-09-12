1c8aa8b709e8ae3436bc34d25b713278545ddc32
Atanas Zashev <atzashev@gmail.com>
2023-09-12 09:53:16 +0300

    Initial commit

---
+++ lib/config/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+double kFontSizeTitle = 24.sp;
+const Color kColorListGreen = Color(0xFF0097A7);
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:emvia_expert/config/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+    return Column(
+      spa,
+      children: [
+        Container(),
+        Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20.h,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
